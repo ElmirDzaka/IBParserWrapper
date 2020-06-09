65624deb1cca6593fb3ac2fdf8363098cb278b45
Elmir Dzaka <43626153+ElmirDzaka@users.noreply.github.com>
2020-06-09 11:42:04 -0600

    Quick rough draft of a script parsing 'Could'

--- PythonScript/PythonWrapperScript.py
+++ PythonScript/PythonWrapperScript.py
@@ -1,1 +1,18 @@
 #Script that parses "could" lines to establish underlinked connections from host/port and port/port connections.
+#import bash file here
+
+#input_file = open('BASHFILE', 'r') 
+
+#set counter to parse each line of file
+#count = 0
+
+#for loop addresses a could in a line, and prints the line that contains the could message
+
+#for line in input_file: 
+  #  if 'could' in line:
+  #      print("Connection could be underlinked on this line: ", line)
+  #  count += 1
+ 
+#closes bash file
+#input_file.close() 
+ 
