a578190c55ac0b5e8dea602ba485564787a1163e
Elmir Dzaka <elmir.dzaka@utah.edu>
2020-06-11 14:43:50 -0600

    adding demo text file to test

--- PythonScript/PythonWrapperScript.py
+++ PythonScript/PythonWrapperScript.py
@@ -1,11 +1,12 @@
 #Script that parses "could" lines to establish underlinked connections from host/port and port/port connections.
 
-#import bash file iblinkinfo.out here
+#import bash file iblinkinfo.out here For now, use demotxt until I copy file to my system
 
-input_file = open('BASHFILE', 'r') 
+input_file = open('demoText.txt', 'r')
 Lines = input_file.readlines()
 
- 
+
+
 #closes bash file
 input_file.close() 
  
